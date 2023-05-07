65a17373d9bb6ca21132729a3f0a706f66215365
Hankyul Min <denirokr22@gmail.com>
2023-05-07 22:28:16 +0900

    [23.05.07 Hankyul] 버튼 클릭시 미세먼지 api 호출하여 표 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,49 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [row, setRow] = useState([]);
+
+  const handleClick = () => {
+    fetch(
+      "http://openapi.seoul.go.kr:8088/54416e584164656e39387a5550424c/json/RealtimeCityAir/1/25"
+    )
+      .then((res2) => res2.json())
+      .then((res3) => {
+        setRow(res3.RealtimeCityAir.row);
+      });
+  };
+
+  console.log(row);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>안녕 리액트</p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>서울시 실시간 대기오염 정보</h1>
+      <button className="fetch-button" onClick={handleClick}>
+        데이터 가져오기
+      </button>
+      <table>
+        <thead>
+          <tr>
+            <th>이름</th>
+            <th>PM10</th>
+            <th>O3</th>
+            <th>상태</th>
+          </tr>
+        </thead>
+        <tbody>
+          {row.map(function (obj) {
+            return (
+              <tr key={obj.MSRSTE_NM}>
+                <td>{obj.MSRSTE_NM}</td>
+                <td>{obj.PM10}</td>
+                <td>{obj.O3}</td>
+                <td>{obj.IDEX_NM}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </>
   );
 }
